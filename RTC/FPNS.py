826f91da2d5cdfb25bb2d0ffefe8ae932f9add82
DerrickYLJ <lijiey@andrew.cmu.edu>
2023-08-07 21:46:12 -0400

    fix index

--- RTC/FPNS.py
+++ RTC/FPNS.py
@@ -34,7 +34,7 @@ def compute_recurrent(maxC, i, I_curr, I_prev, execution_list, period_list, coun
 
 def worst_case_compute(index_list, execution_list, period_list):
     result = np.zeros(len(index_list))
-    for i in index_list[:(len(index_list)-1)]:
+    for i in range(len(index_list)-1):
         i = i-1
         maxC = max(execution_list[(i+1):])
         I = execution_list[i]+compute_recurrent(maxC, i, 0, 0, execution_list, period_list, 0)
@@ -46,12 +46,15 @@ def process_input_files(folder_path, writer):
     for filename in os.listdir(folder_path):
         if filename.endswith('.csv'):
             file_path = os.path.join(folder_path, filename)
+            print(f"Calculating the worst case delay of this architecture: {file_path}. ")
             streams = read_input_file(file_path)
             index_list = streams[:,0]
             execution_list = streams[:,1]
             period_list = streams[:,2]
             result_list = worst_case_compute(index_list, execution_list, period_list)
-            writer.writerow(result_list)
+            for i in range(len(result_list)):
+                print(f"stream index: {index_list[i]}, worst case delay: {result_list[i]}")
+                writer.writerow([index_list[i],result_list[i]])
             
                 
 if __name__ == "__main__":
