1dc6e82957768ab0c716729976f8167c635d7117
DerrickYLJ <lijiey@andrew.cmu.edu>
2023-11-08 01:00:06 -0500

    update for FPNS

--- ResTimeAnalysis/FPNS.py
+++ ResTimeAnalysis/FPNS.py
@@ -29,40 +29,42 @@ def compute_recurrent(maxC, i, I_curr, I_prev, execution_list, period_list, coun
     if I_curr == I_prev and (count != 0):
         return I_curr
     else:
-        summation = 0
-        if i != 0:
-            summation =  np.sum(np.dot(np.int_(I_curr/period_list)+1,execution_list))
+        summation =  np.sum(np.dot(np.int_(I_curr/period_list)+1,execution_list))
         I_prev = I_curr
         I_curr = maxC+summation
         return compute_recurrent(maxC, i, I_curr, I_prev, execution_list, period_list, count+1)
 
-def worst_case_compute(index_list, execution_list, period_list):
+def worst_case_compute(index_list, execution_list, period_list, priority_list):
     result = np.zeros(len(index_list))
     print(index_list[:(len(index_list)-1)])
     for i in range(len(index_list)):
         curr_index = index_list[i]
         input_executaion_list=[]
         input_period_list=[]
-        # Start exclusing streams that have blocked before
-        if i != 0:
-            for j in range(i):
-                if index_list[j] in stream_dictionary[curr_index]:
-                    continue
-                else:
-                    input_executaion_list.append(execution_list[j])
-                    input_period_list.append(period_list[j])
-                    stream_dictionary[curr_index].add(index_list[j])
+        
+        # Take the maximum of all? (consider the same priority or not)
         maxC = 0
-        print(stream_dictionary)
         if i != len(index_list)-1:
+            maxind = -1
             for j in range(len(index_list)):
                 ind = index_list[j]
-                if j <= i:
-                    continue
-                else:
+                if i != j and priority_list[j] < priority_list[i]:
                     if ind not in stream_dictionary[curr_index]:
+                        maxind = ind if execution_list[j] > maxC else maxind
                         maxC = execution_list[j] if execution_list[j] > maxC else maxC
-                stream_dictionary[curr_index].add(ind)
+            stream_dictionary[curr_index].add(maxind)
+
+        # Start adding streams to the summation term
+
+        for j in range(len(index_list)):
+            # Only lower priority 
+            if i != j and index_list[j] not in stream_dictionary[curr_index] and priority_list[j] >= priority_list[i]:
+                input_executaion_list.append(execution_list[j])
+                input_period_list.append(period_list[j])
+            stream_dictionary[curr_index].add(index_list[j])
+        
+        print(stream_dictionary)
+        
 
             # maxC = max(execution_list[(i+1):])
             # maxC = max(input_executaion_list) if len(input_executaion_list) != 0 else 0
@@ -85,10 +87,10 @@ def process_input_files(folder_path, writer):
                     continue
                 streams = read_input_file(file_path)
                 index_list = streams[:,0]
-                print(streams)
                 execution_list = streams[:,1]
                 period_list = streams[:,2]
-                result_list = worst_case_compute(index_list, execution_list, period_list)
+                priority_list = streams[:,3]
+                result_list = worst_case_compute(index_list, execution_list, period_list, priority_list)
                 for i in range(len(result_list)):
                     print(f"stream index: {index_list[i]}, worst case delay: {result_list[i]}")
                     writer.writerow([index_list[i],result_list[i]])
@@ -100,7 +102,7 @@ def update_final_result(index, delay):
 if __name__ == "__main__":
     for count in range(72):
         stream_dictionary = { (i+1):set() for i in range(13)}
-        folder_path = f"Folder_{count+1}"
+        folder_path = f"Permutations/Folder_{count+1}"
         file_name = f"File_{count+1}.csv"
         file_path = os.path.join(folder_path, file_name)
         dic={}
@@ -129,7 +131,7 @@ if __name__ == "__main__":
     mykeys = list(final_result.keys())
     mykeys.sort()
     sorted_dict = [(key, final_result[key]) for key in myKeys]
-    print(sorted_dict)
+    print([value for (key, value) in sorted_dict])
     plt.plot([key for (key, value) in sorted_dict], [value for (key, value) in sorted_dict], "*-")
     plt.plot([key for (key, value) in sorted_dict], [205, 235, 408, 310, 175, 197, 404, 382, 618, 362, 580, 208, 422], "o-")
     plt.show()
